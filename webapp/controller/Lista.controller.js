701d7ca5c837c6083bc658eae6886c50ef61cad2
Wesley Felix <wesleyfelix.developer@gmail.com>
2024-03-05 18:58:36 +0000

    Filter Finalizado

--- webapp/controller/Lista.controller.js
+++ webapp/controller/Lista.controller.js
@@ -24,14 +24,22 @@ sap.ui.define([
             onSearch: function(){
                 var oProdutoInput       = this.getView().byId("productIdIput");
                 var oProdutoNameInput   = this.getView().byId("productNameIput");
+                var oProdutoCategoryInput   = this.getView().byId("productCategoryIput");
  
-                var oFilter = new Filter({
-                    filters: [
-                        new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()),
-                        new Filter("Name", FilterOperator.Contains, oProdutoNameInput.getValue())
-                    ],
-                    and: true
-                });
+                var objFilter = { filters: [], and: true };
+                objFilter.filters.push(new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()));
+                objFilter.filters.push(new Filter("Name", FilterOperator.Contains, oProdutoNameInput.getValue()));
+                objFilter.filters.push(new Filter("Category", FilterOperator.Contains, oProdutoCategoryInput.getValue()));
+
+                var oFilter = new Filter(objFilter);
+
+                // var oFilter = new Filter({
+                //     filters: [
+                //         new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()),
+                //         new Filter("Name", FilterOperator.Contains, oProdutoNameInput.getValue())
+                //     ],
+                //     and: true
+                // });
  
                 var oTable = this.getView().byId("tableProdutos");
                 var oBinding = oTable.getBinding("items");
@@ -85,6 +93,24 @@ sap.ui.define([
 
                 var oFilter = new Filter(objFilter);
                 oEvent.getSource().getBinding("items").filter(oFilter);
+            },
+
+            onValueHelpClose: function (oEvent) {
+                var oSelectedItem = oEvent.getParameter("selectedItem");
+                var oInput = null;
+
+                if (this.byId(this._oInput)) {
+                    oInput = this.byId(this._oInput);
+                } else {
+                    oInput = sap.ui.getCore().byId(this._oInput)                    
+                }
+
+                if (!oSelectedItem) {
+                    oInput.resetProperty("value");
+                    return;
+                }
+
+                oInput.setValue(oSelectedItem.getTitle());
             }
         });
     });
